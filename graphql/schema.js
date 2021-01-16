49dbb7b998f4141480b949d6b5e3c251b0f1952b
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-16 17:06:22 -0500

    query para obtener clientes

--- graphql/schema.js
+++ graphql/schema.js
@@ -8,6 +8,9 @@ const typeDefs = gql`
         #Productos
         obtenerProductos: [Producto],
         obtenerProducto(id: ID!): Producto,
+
+        #Clientes
+        obtenerClientes: [Cliente]
     }
 
     type Mutation {
