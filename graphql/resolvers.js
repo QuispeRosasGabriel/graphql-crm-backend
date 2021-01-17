ade50a9850fba7e08513b6b4810e4a012a3741af
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-17 17:29:45 -0500

    validando resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -49,6 +49,17 @@ const resolvers = {
             } catch (error) {
                 console.log(error);
             }
+        },
+        obtenerCliente: async(_, {id}, ctx) => {
+            const cliente = await Cliente.findById(id);
+            if(!cliente) {
+                throw new Error('Cliente no encontrado');
+            }
+
+            if(cliente.vendedor.toString() !== ctx.usuario.id) {
+                throw new Error('No tienes las credenciales');
+            }
+            return cliente;
         }
     },
     Mutation: {
